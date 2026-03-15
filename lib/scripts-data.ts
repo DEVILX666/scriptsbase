b6b61085cd41d94b550dca5ae2a215c445f0f0ff
DEVILX666 <batatabatatax5@gmail.com>
2026-03-15 20:47:07 +0000

    Update scripts-data.ts

--- lib/scripts-data.ts
+++ lib/scripts-data.ts
@@ -14,7 +14,7 @@ export const gameScripts: GameScript[] = [
     description: "",
     category: "Adventure",
     features: ["Auto Steal", "Item ESP", "Speed Hack", "TELEPORT", "FLY"],
-    logoUrl: "https://tr.rbxcdn.com/180DAY-d3ad1abdc26ef3d577703f55bfe34450/150/150/Image/Webp/noFilter",
+    logoUrl: "https://tr.rbxcdn.com/180DAY-ade5155afe23aa0b3ccedbd1b456ac37/150/150/Image/Webp/noFilter",
   },
   {
     id: "plants-vs-brainrots",
@@ -22,7 +22,7 @@ export const gameScripts: GameScript[] = [
     description: "",
     category: "Other",
     features: ["Item Dupe", "Auto Farm & Auto Buy", "Auto-Attack", "Kill-Aura", "No Cooldown"],
-    logoUrl: "https://tr.rbxcdn.com/180DAY-63a9dbc67a5af8852833e3dce26593a1/150/150/Image/Webp/noFilter",
+    logoUrl: "https://tr.rbxcdn.com/180DAY-3b569ae77d5b5b22d251745dbb456cad/150/150/Image/Webp/noFilter",
   },
   {
     id: "grow-garden",
